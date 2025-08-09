b8ae245d72ad954ce3b95672ddfb556078ae1951
GenChadT <git+github@timothywb.com>
2025-08-09 17:31:41 -0400

    Add configuration file and implement main application menu

--- src/app.py
+++ src/app.py
@@ -42,6 +42,47 @@ class PrinterPingerApp:
         
         # --- UI Setup ---
         self._setup_ui(browser_name)
+        self._setup_menu()
+
+    def _setup_menu(self):
+        """Creates the main application menu bar."""
+        self.menu_bar = tk.Menu(self.root)
+        self.root.config(menu=self.menu_bar)
+
+        # --- File Menu ---
+        file_menu = tk.Menu(self.menu_bar, tearoff=0)
+        self.menu_bar.add_cascade(label="File", menu=file_menu, underline=0)
+        file_menu.add_command(label="New", underline=0)
+        file_menu.add_command(label="Open", underline=0)
+        file_menu.add_command(label="Save", underline=0)
+        file_menu.add_command(label="Save As", underline=5)
+
+        # --- Edit Menu ---
+        edit_menu = tk.Menu(self.menu_bar, tearoff=0)
+        self.menu_bar.add_cascade(label="Edit", menu=edit_menu, underline=0)
+        edit_menu.add_command(label="Clear", underline=0)
+
+        # --- View Menu ---
+        view_menu = tk.Menu(self.menu_bar, tearoff=0)
+        self.menu_bar.add_cascade(label="View", menu=view_menu, underline=0)
+        view_menu.add_command(label="Zoom In", underline=5)
+        view_menu.add_command(label="Zoom Out", underline=5)
+        view_menu.add_separator()
+        view_menu.add_checkbutton(label="Show Menubar", underline=5)
+        view_menu.add_checkbutton(label="Show Statusbar", underline=5)
+
+        # --- Settings Menu ---
+        settings_menu = tk.Menu(self.menu_bar, tearoff=0)
+        self.menu_bar.add_cascade(label="Settings", menu=settings_menu, underline=0)
+        settings_menu.add_command(label="Preferences", underline=0)
+
+        # --- Help Menu ---
+        help_menu = tk.Menu(self.menu_bar, tearoff=0)
+        self.menu_bar.add_cascade(label="Help", menu=help_menu, underline=0)
+        help_menu.add_command(label="Check for Updates", underline=6)
+        help_menu.add_separator()
+        help_menu.add_command(label="Github", underline=0)
+        help_menu.add_command(label="About", underline=0)
 
     def _setup_ui(self, browser_name: str):
         """Creates and configures the UI elements."""
